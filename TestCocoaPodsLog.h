066fdf6f372e78bab49ee6457217a167a83889af
lovexiaoping <love_ping891122@163.com>
2016-08-16 21:27:48 +0800

    1.0
    init

---
+++ TestCocoaPodsLog.h
@@ -0,0 +1,14 @@
+//
+//  TestCocoaPodsLog.h
+//  MyFrameWork
+//
+//  Created by 丁浪平 on 16/8/16.
+//  Copyright © 2016年 dlp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestCocoaPodsLog : NSObject
+
++(void)printTestCocoaPodsLog:(NSString *)name;
+@end
---
+++ TestCocoaPodsLog.m
@@ -0,0 +1,17 @@
+//
+//  TestCocoaPodsLog.m
+//  MyFrameWork
+//
+//  Created by 丁浪平 on 16/8/16.
+//  Copyright © 2016年 dlp. All rights reserved.
+//
+
+#import "TestCocoaPodsLog.h"
+
+@implementation TestCocoaPodsLog
++(void)printTestCocoaPodsLog:(NSString *)name{
+
+    NSLog(@"printTestCocoaPodsLog: %@",name);
+
+}
+@end
